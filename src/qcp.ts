88e691b963e989f658678324576631898c198a81
Johnny Stenson <johnjstenson@gmail.com>
2020-03-31 19:15:18 -0400

    initial commit

---
+++ src/qcp.ts
@@ -0,0 +1,74 @@
+
+// SET TO FALSE IN PRODUCTION
+const DEBUG = true;
+
+function debug(...args) {
+  if (DEBUG) {
+    console.log(...args);
+  }
+}
+
+/**
+ * This method is called by the calculator when the plugin is initialized.
+ * @param {QuoteLineModel[]} quoteLineModels An array containing JS representations of all lines in a quote
+ * @returns {Promise}
+ */
+export function onInit(quoteLineModels) {
+  return new Promise((resolve, reject) => {
+    // Perform logic here and resolve promise
+    resolve();
+  });
+}
+
+/**
+ * This method is called by the calculator before calculation begins, but after formula fields have been evaluated.
+ * @param {QuoteModel} quoteModel JS representation of the quote being evaluated
+ * @param {QuoteLineModel[]} quoteLineModels An array containing JS representations of all lines in the quote
+ * @returns {Promise}
+ */
+export function onBeforeCalculate(quoteModel, quoteLineModels) {
+  return new Promise((resolve, reject) => {
+    // Perform logic here and resolve promise
+    resolve();
+  });
+}
+
+function mapRecords(quoteLineModels) {
+  return quoteLineModels.map(model => model.record);
+}
+
+function logRecords(quoteOrLineModel) {
+  // serializing records removes proxy to make debugging easier,
+  // BUT is a performance hit, so make sure to disable logging in production to avoid this without code changes
+  if (DEBUG) {
+    const models = Array.isArray(quoteOrLineModel) ? quoteOrLineModel : [quoteOrLineModel];
+    debug(JSON.parse(JSON.stringify(mapRecords(models))));
+  }
+}
+
+/**
+ * Group all quote lines by very top level bundle
+ * This is useful when you need to rollup values on a bundle by bundle basis
+ */
+function groupByTopLevelBundle(quoteLineModels) {
+  const bundles = quoteLineModels.reduce((bundles, line) => {
+    const parentKey = getParentKey(line);
+    if (!bundles[parentKey]) {
+      bundles[parentKey] = [];
+    }
+    bundles[parentKey].push(line);
+    return bundles;
+  }, {});
+
+  debug('bundles', bundles);
+  return bundles;
+}
+
+/** recursively get parent key */
+function getParentKey(quoteLine) {
+  if (quoteLine.parentItem) {
+    return getParentKey(quoteLine.parentItem);
+  } else {
+    return quoteLine.key;
+  }
+}
