efc0fdcbce2a1f8de94dd63120613ebf58f4157a
Johnny Stenson <johnjstenson@gmail.com>
2020-04-15 12:39:43 -0400

    custom package total

--- src/qcp.ts
+++ src/qcp.ts
@@ -115,7 +115,10 @@ function calc_CMU_BLOCK(quoteLineModels){
   var rows_CMU_GROUT_SOLID = [];
   var line_CMU_GROUT_SOLID = [];
   var line_CMU_GROUT_REBAR = [];
-
+  var rows_CMU_BOND_BEAM = [];
+  var line_CMU_BOND_BEAM = [];
+  var cost_CMU_BOND_BEAM = 0;
+  var line_CMU_BOND_Quantity = [];
 
   if (quoteLineModels != null) {
     quoteLineModels.forEach(function(line) {
@@ -125,13 +128,24 @@ function calc_CMU_BLOCK(quoteLineModels){
       /* Cost Inputs */
       if(line.record['SBQQ__ProductCode__c'] === 'CONCRETE_3000_PY_COST'){ costConcretePY = line.record['SBQQ__UnitCost__c']; }
       if(line.record['SBQQ__ProductCode__c'] === 'CONCRETE_DELIVERY'){ costConcreteDelivery = line.record['SBQQ__UnitCost__c']; }
+      if(line.record['SBQQ__ProductCode__c'] === 'CMU_ROWS_BOND_BEAM'){ cost_CMU_BOND_BEAM = line.record['SBQQ__UnitCost__c']; }
+
+      /* Parent Block Quote Template Line Items Section */
+      var filterLine_CMU_BLOCK_IN = line.record['SBQQ__ProductCode__c'].substring(0,10) + line.record['SBQQ__ProductCode__c'].slice(line.record['SBQQ__ProductCode__c'].length - 2);
+      if(filterLine_CMU_BLOCK_IN === 'CMU_BLOCK_IN'){line.record['Quote_Line_Item_Section__c'] = 'Block'}
 
       if (parent != null) {
         var parentKey = parent.key;
+        var filterPC_CMU_BLOCK_IN = parent.record['SBQQ__ProductCode__c'].substring(0,10) + parent.record['SBQQ__ProductCode__c'].slice(parent.record['SBQQ__ProductCode__c'].length - 2);
+        
+
+        /* Block Quote Template Line Items Section */
+        if(filterPC_CMU_BLOCK_IN === 'CMU_BLOCK_IN'){line.record['Quote_Line_Item_Section__c'] = 'Block'}
 
         /* Quantity of Block */
-        var filterPC_CMU_BLOCK_IN = parent.record['SBQQ__ProductCode__c'].substring(0,10) + parent.record['SBQQ__ProductCode__c'].slice(parent.record['SBQQ__ProductCode__c'].length - 2);
-
+        if(filterPC_CMU_BLOCK_IN === 'CMU_BLOCK_IN' && line.record['SBQQ__ProductCode__c'].substring(0, 16) === 'CMU_BLOCK_QUANT_'){
+          line_CMU_BOND_Quantity[parentKey] = line;
+        }
         if(filterPC_CMU_BLOCK_IN === 'CMU_BLOCK_IN' && line.record['SBQQ__ProductCode__c'] === 'CMU_LF'){
           cmuLF[parentKey] = line.record['SBQQ__Quantity__c'].valueOf();
           parent_CMU_BLOCK[parentKey] = parent;
@@ -164,7 +178,7 @@ function calc_CMU_BLOCK(quoteLineModels){
           line_V_REBAR_BAR[parent.parentItem.key] = line;
         }
         
-        /* Solidf Grout */
+        /* Solid Grout */
         if(line.record['SBQQ__ProductCode__c'] === 'CMU_GROUT_SOLID'){
           line_CMU_GROUT_SOLID[parentKey] = line;
           rows_CMU_GROUT_SOLID[parentKey] = line.record['SBQQ__Quantity__c'].valueOf();
@@ -174,14 +188,24 @@ function calc_CMU_BLOCK(quoteLineModels){
         if(line.record['SBQQ__ProductCode__c'] === 'CMU_GROUT_SOLID_V_REBAR'){
           line_CMU_GROUT_REBAR[parentKey] = line;
         }
+
+        /* Bond Beam */
+        if(line.record['SBQQ__ProductCode__c'] === 'CMU_ROWS_BOND_BEAM'){
+          rows_CMU_BOND_BEAM[parentKey] = line.record['SBQQ__Quantity__c'].valueOf();
+          line_CMU_BOND_BEAM[parentKey] = line;
+        }
       }
     }); // END OF LINES
 
     
     if(parent_CMU_BLOCK){
       parent_CMU_BLOCK.forEach(function(parent_line, key) {
+        /* Reset Package Total on Block */
+        parent_line.record['Custom_Package_Total__c'] = 0;
+
         /* Quantity of Block */
         parent_line.record['SBQQ__Quantity__c'] = Math.ceil(cmuLF[key] * courses[key] / 1.33);
+        line_CMU_BOND_Quantity[key].record['SBQQ__Quantity__c'] = Math.ceil(cmuLF[key] * courses[key] / 1.33);
 
         /* Durawall */
         if(line_CMU_DURAWALL[key]){
@@ -199,7 +223,10 @@ function calc_CMU_BLOCK(quoteLineModels){
           }else{
             tmpVRebarBars = Math.ceil(tmpHeight * tmpQtyRebar / 20);
           }
-          line_V_REBAR_BAR[key].record['SBQQ__Quantity__c'] = tmpVRebarBars;
+          parent_CMU_V_REBAR[key].record['SBQQ__Quantity__c'] = tmpVRebarBars;
+          parent_CMU_V_REBAR[key].record['SBQQ__NetPrice__c'] = line_V_REBAR_BAR[key].record['SBQQ__ListPrice__c'];
+          line_V_REBAR_BAR[key].record['SBQQ__Quantity__c'] = 0;
+          //parent_CMU_V_REBAR[key].record['SBQQ__PackageTotal__c'] = parent_CMU_V_REBAR[key].record['SBQQ__NetTotal__c'];
         }
 
         /* Solid Grout */
@@ -216,10 +243,43 @@ function calc_CMU_BLOCK(quoteLineModels){
           var tmpPriceGrout = tmpRebarFillYards * costConcretePY + Math.ceil(tmpRebarFillYards / 10) * costConcreteDelivery;
           line_CMU_GROUT_REBAR[key].record['SBQQ__NetPrice__c'] = tmpPriceGrout;
         }
+
+        /* Bond Beam */
+        if(line_CMU_BOND_BEAM[key]){
+          var tmpNoBondBeamBlocks = Math.ceil(cmuLF[key] / 1.33) * rows_CMU_BOND_BEAM[key];
+          line_CMU_BOND_BEAM[key].record['SBQQ__NetPrice__c'] = tmpNoBondBeamBlocks * cost_CMU_BOND_BEAM / rows_CMU_GROUT_SOLID[key];
+          //console.log(tmpNoBondBeamBlocks * cost_CMU_BOND_BEAM / rows_CMU_GROUT_SOLID[key]);
+        }
+
+
+        /**
+         * SF PAckage Total Question
+         * https://success.salesforce.com/0D53A00004s13b8
+         */
+        /* Roll Up Package Total to Parent */
+        quoteLineModels.forEach(function(line) {
+          var parent = line.parentItem;
+          if(parent){
+            var filterPC_CMU_BLOCK_IN = parent.record['SBQQ__ProductCode__c'].substring(0,10) + parent.record['SBQQ__ProductCode__c'].slice(parent.record['SBQQ__ProductCode__c'].length - 2);
+
+            if(filterPC_CMU_BLOCK_IN === 'CMU_BLOCK_IN'){
+              /* Compute line.CPT */
+              line.record['Custom_Package_Total__c'] = line.record['SBQQ__Quantity__c'] * line.record['SBQQ__NetPrice__c'];
+
+              /* Add line.CPT to parent.CPT */
+              parent.record['Custom_Package_Total__c'] = parent.record['Custom_Package_Total__c'] + line.record['Custom_Package_Total__c'];
+
+              console.log('childCPT ' + line.record['SBQQ__ProductName__c'] + ': ' + line.record['Custom_Package_Total__c']);
+              console.dir(line);
+              console.log('parentCPT ' + parent.record['SBQQ__ProductName__c'] + ': ' + parent.record['Custom_Package_Total__c']);
+              console.dir(parent);
+            } //is line duping?
+          }
+        });
       });
     }
 
-    /* DEBUG TO CONSOLE */
+    /* DEBUG TO CONSOLE 
     console.log('DEBUG:');
     console.dir(parent_CMU_BLOCK);
     console.dir(inchBlock);
@@ -237,7 +297,10 @@ function calc_CMU_BLOCK(quoteLineModels){
     console.dir(rows_CMU_GROUT_SOLID);
     console.dir(line_CMU_GROUT_SOLID);
     console.dir(line_CMU_GROUT_REBAR);
-    
+    console.dir(rows_CMU_BOND_BEAM);
+    console.dir(line_CMU_BOND_BEAM);
+    console.log(cost_CMU_BOND_BEAM);
+*/
   }
 
 
@@ -250,7 +313,7 @@ function calc_CMU_BLOCK(quoteLineModels){
  * @returns double factor to be used in Rebar Cells Fill Yards formula
  */
 function factorRebarCellsFillYards(inchBlock){
-  console.log("switch:" + inchBlock);
+  //console.log("switch:" + inchBlock);
   switch(inchBlock) {
     case 4:
       return .06;
@@ -279,7 +342,7 @@ function factorRebarCellsFillYards(inchBlock){
  * @returns double factor to be used in Grout Solid Fill Yards formula
  */
 function factorBlockFillYards(inchBlock){
-  console.log("switch:" + inchBlock);
+  //console.log("switch:" + inchBlock);
   switch(inchBlock) {
     case 4:
       return .09;
